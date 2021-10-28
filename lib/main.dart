3da675dc0a361d359e07944d5d9f8e204d421c84
Anfacibry <jogos.fabriciocintra@gmail.com>
2021-10-28 15:25:20 -0300

    atualizando main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/pages/paginas_grides_produtos.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,22 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("shop"),
-        centerTitle: true,
-      ),
-      body: Container(),
+      home: PaginaGrideProdutos(),
     );
   }
 }
