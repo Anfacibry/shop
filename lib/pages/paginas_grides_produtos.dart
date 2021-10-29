ebe77cac54bd36a1be5ab8e698c49404efe59f1d
Anfacibry <jogos.fabriciocintra@gmail.com>
2021-10-29 14:42:38 -0300

    adicionando paginas de gridas com produtos

--- lib/pages/paginas_grides_produtos.dart
+++ lib/pages/paginas_grides_produtos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/components/itens_produtos.dart';
 import 'package:shop/data/dados_produtos.dart';
 import 'package:shop/models/produtos.dart';
 
@@ -13,17 +14,15 @@ class PaginaGrideProdutos extends StatelessWidget {
         title: const Text("Minha Loja"),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: GridView.builder(
-          itemCount: listaProdutos.length,
-          itemBuilder: (ctx, index) => Text(listaProdutos[index].titulo!),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, //Quantidade de cada elemento em cada linha
-            childAspectRatio: 3 / 2, //Relação entre altura e lagura
-            crossAxisSpacing: 10, // Espaçamento no eixo cruzado
-            mainAxisSpacing: 10, // Espaçamento no eixo principal
-          ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(20),
+        itemCount: listaProdutos.length,
+        itemBuilder: (ctx, index) => ItensProdutos(listaProdutos[index]),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, //Quantidade de cada elemento em cada linha
+          childAspectRatio: 3 / 2, //Relação entre altura e lagura
+          crossAxisSpacing: 10, // Espaçamento no eixo cruzado
+          mainAxisSpacing: 10, // Espaçamento no eixo principal
         ),
       ),
     );
